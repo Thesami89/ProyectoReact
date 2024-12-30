0d8e2636cd877cd9054dd04d64f7b30d5c713c67
samuel <orozcos881@gmail.com>
2024-12-29 22:24:13 -0600

    actualizacion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@
 import Header from './components/header/header';
 import Formulario from './components/Formluario/formulario';
 import MiOrg from './components/Miorg/Miorg';
+import { useState } from 'react';
 
 function App() {
+  
+  const [mostrarFormlulario, actualizarMostrar] = useState(true);
+
+  const cambiarMostrar = () =>{
+    actualizarMostrar(!mostrarFormlulario) //Lo que hacemos es que la funcion actuializar actualiza el valor de la cajita y nosotros le  mnadamos el nuevo rsultuado
+  }
+
+  //Recordatorio: operador ternario = Ternario = condicion ? Mostrar : NoMostrar
+  //Tambien se puede representar de la forma de condicion && mostrar
+
   return (
     <div>
       
       <Header />
-      <Formulario />
-      <MiOrg />
+      {/* {mostrarFormlulario == true ? <Formulario/> : <div></div>} */}
+      {mostrarFormlulario && <Formulario/> }
+      <MiOrg cambiarMostrar = {cambiarMostrar}/> {/* Las funciones se pueden mandar por medio de props con la siguiente sintaxis que se muestra ennpanatalla  */}
 
       
       
